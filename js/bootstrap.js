cf24a86123c370d57e39a149412fb0fa7c9465f5
Blake Griffith <blake.a.griffith@gmail.com>
2024-11-13 15:56:25 -0500

    log bs nodes length

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -7,4 +7,4 @@ const node = DHT.bootstrapper(10001, '127.0.0.1', {...opts})
 await node.fullyBootstrapped();
 console.log('boooootd');
 
-setInterval(() => console.log(node.nodes), 5*1000);
+setInterval(() => console.log(node.nodes.length), 5*1000);
