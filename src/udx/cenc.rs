a44b5bc0c8566840309dd39dd5ab0c7ea111a84a
Blake Griffith <blake.a.griffith@gmail.com>
2024-11-19 13:16:07 -0500

    Add ReqMsgData

--- src/udx/cenc.rs
+++ src/udx/cenc.rs
@@ -6,7 +6,7 @@ use std::{
 use compact_encoding::{CompactEncoding, EncodingError, State};
 
 use crate::{
-    constants::{HASH_SIZE, ID_SIZE},
+    constants::{HASH_SIZE, ID_SIZE, REQUEST_ID, RESPONSE_ID},
     udx::{Addr, Command},
     Error, Result,
 };
@@ -252,4 +252,240 @@ pub fn decode_reply(buff: &[u8], mut from: Addr, state: &mut State) -> Result<Re
         error,
         value,
     })
+}
+pub struct RequestMsgData {
+    pub tid: u16,
+    pub to: Addr,
+    pub id: Option<[u8; 32]>,
+    pub internal: bool,
+    pub token: Option<[u8; 32]>,
+    pub command: Command,
+    pub target: Option<[u8; 32]>,
+    pub value: Option<Vec<u8>>,
+}
+impl RequestMsgData {
+    fn encode(&self) -> Result<Vec<u8>> {
+        let mut state = State::new();
+        state.add_end(1 + 1 + 6 + 2)?;
+
+        if self.id.is_some() {
+            state.add_end(ID_SIZE)?;
+        }
+        if self.token.is_some() {
+            state.add_end(32)?;
+        }
+
+        let cmd = self.command.clone() as usize;
+        state.preencode_usize_var(&cmd)?;
+
+        if self.target.is_some() {
+            state.add_end(32)?;
+        }
+
+        if let Some(v) = &self.value {
+            state.preencode_buffer(v)?;
+        }
+
+        let mut buff = state.create_buffer();
+
+        buff[state.start()] = REQUEST_ID;
+        state.add_start(1)?;
+
+        let mut flags: u8 = 0;
+        if self.id.is_some() {
+            flags |= 1 << 0;
+        }
+        if self.token.is_some() {
+            flags |= 1 << 1;
+        }
+        if self.internal {
+            flags |= 1 << 2;
+        }
+        if self.target.is_some() {
+            flags |= 1 << 3;
+        }
+        if self.value.is_some() {
+            flags |= 1 << 4;
+        }
+        buff[state.start()] = flags;
+        state.add_start(1)?;
+
+        state.encode_u16(self.tid, &mut buff)?;
+
+        state.encode(&self.to, &mut buff)?;
+
+        if let Some(id) = &self.id {
+            state.encode_fixed_32(id, &mut buff)?;
+        }
+        if let Some(t) = self.token {
+            // c.fixed32.encode(state, token)
+            state.encode_fixed_32(&t, &mut buff)?;
+        }
+
+        buff[state.start()] = self.command.clone() as u8;
+        state.add_start(1)?;
+
+        // c.uint.encode(state, this.command)
+        if let Some(t) = &self.target {
+            // c.fixed32.encode(state, this.target)
+            state.encode_fixed_32(t, &mut buff)?;
+        }
+        if let Some(v) = &self.value {
+            state.encode_buffer(&v, &mut buff)?;
+        }
+        Ok(buff.to_vec())
+    }
+    fn decode(buff: &[u8], state: &mut State) -> Result<RequestMsgData> {
+        let flags = buff[state.start()];
+        state.add_start(1)?;
+
+        let tid = state.decode_u16(buff)?;
+
+        let to = state.decode(buff)?;
+
+        let id = decode_fixed_32_flag(flags, 1, state, buff)?;
+        let token = decode_fixed_32_flag(flags, 2, state, buff)?;
+
+        let internal = (flags & 4) != 0;
+        let command: Command = state.decode(buff)?;
+
+        let target = decode_fixed_32_flag(flags, 8, state, buff)?;
+
+        let value = if flags & 16 > 0 {
+            Some(state.decode_buffer(buff)?.as_ref().to_vec())
+        } else {
+            None
+        };
+
+        Ok(RequestMsgData {
+            tid,
+            to,
+            id,
+            internal,
+            token,
+            command,
+            target,
+            value,
+        })
+    }
+}
+
+pub struct ReplyMsgData {
+    pub tid: u16,
+    pub to: Addr,
+    pub id: Option<[u8; 32]>,
+    pub token: Option<[u8; 32]>,
+    pub closer_nodes: Vec<Addr>,
+    pub error: usize,
+    pub value: Option<Vec<u8>>,
+}
+
+impl ReplyMsgData {
+    fn encode(&self) -> Result<Vec<u8>> {
+        let mut state = State::new();
+        // (type | version) + flags + to + tid
+        state.add_end(1 + 1 + 6 + 2)?;
+        if self.id.is_some() {
+            state.add_end(32)?;
+        }
+        if self.token.is_some() {
+            state.add_end(32)?;
+        }
+        if !self.closer_nodes.is_empty() {
+            state.preencode_usize_var(&self.closer_nodes.len())?;
+            for n in &self.closer_nodes {
+                state.preencode(n)?;
+            }
+        }
+        if self.error > 0 {
+            state.preencode(&self.error)?;
+        }
+        if let Some(v) = &self.value {
+            state.preencode(v)?;
+        }
+
+        let mut buff = state.create_buffer();
+        buff[state.start()] = RESPONSE_ID;
+        state.add_start(1)?;
+
+        let mut flags: u8 = 0;
+
+        if self.id.is_some() {
+            flags |= 1 << 0;
+        }
+        if self.token.is_some() {
+            flags |= 1 << 1;
+        }
+        if !self.closer_nodes.is_empty() {
+            flags |= 1 << 2;
+        }
+        if self.error > 0 {
+            flags |= 1 << 3;
+        }
+        if self.value.is_some() {
+            flags |= 1 << 4;
+        }
+        buff[state.start()] = flags;
+
+        state.encode_u16(self.tid, &mut buff)?;
+        state.encode(&self.to, &mut buff)?;
+
+        if let Some(id) = &self.id {
+            state.encode_fixed_32(id, &mut buff)?;
+        }
+        if let Some(token) = self.token {
+            state.encode_fixed_32(&token, &mut buff)?;
+        }
+        if !self.closer_nodes.is_empty() {
+            state.encode_usize_var(&self.closer_nodes.len(), &mut buff)?;
+            for n in &self.closer_nodes {
+                state.encode(n, &mut buff)?;
+            }
+        }
+        if self.error > 0 {
+            state.encode(&self.error, &mut buff)?;
+        }
+        if let Some(value) = &self.value {
+            state.encode_fixed_32(value, &mut buff)?;
+        }
+        Ok(buff.into())
+    }
+
+    fn decode(buff: &[u8], state: &mut State) -> Result<ReplyMsgData> {
+        let flags = buff[state.start()];
+        state.add_start(1)?;
+
+        let tid = state.decode_u16(buff)?;
+        let to: Addr = state.decode(buff)?;
+
+        let id = decode_fixed_32_flag(flags, 1, state, buff)?;
+        let token = decode_fixed_32_flag(flags, 2, state, buff)?;
+
+        let closer_nodes: Vec<Addr> = if flags & 4 > 0 {
+            decode_addr_array(state, buff)?
+        } else {
+            vec![]
+        };
+
+        let error = if flags & 8 > 0 {
+            state.decode_usize_var(buff)?
+        } else {
+            0
+        };
+
+        let value = if flags & 16 > 0 {
+            Some(state.decode_buffer(buff)?.to_vec())
+        } else {
+            None
+        };
+        Ok(ReplyMsgData {
+            tid,
+            to,
+            id,
+            token,
+            closer_nodes,
+            error,
+            value,
+        })
+    }
 }
