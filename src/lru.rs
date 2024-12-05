688d11150af096ea0422511fd6f7a738ae02bf30
Blake Griffith <blake.a.griffith@gmail.com>
2024-12-05 17:34:10 -0500

    clippy allow manual_inspect

--- src/lru.rs
+++ src/lru.rs
@@ -399,6 +399,8 @@ impl PeerCache {
 
         let expiration = now + self.age;
         let list = &mut self.list;
+        // see: https://github.com/rust-lang/rust-clippy/issues/13185
+        #[allow(clippy::manual_inspect)]
         self.map.get_mut(key).map(|addrs| {
             Self::update_key(list, key);
             addrs.set_expiration(expiration);
