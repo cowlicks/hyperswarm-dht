c48d4c43bfbd04633913dbf2e3810cdd5c4ad402
Blake Griffith <blake.a.griffith@gmail.com>
2025-02-09 14:31:08 -0500

    split out testnet tests

--- hdht/tests/js_integration.rs
+++ hdht/tests/js_integration.rs
@@ -1,29 +1,22 @@
 mod common;
 use std::net::SocketAddr;
 
-use dht_rpc::{DhtConfig, IdBytes};
-use futures::StreamExt;
+use dht_rpc::IdBytes;
 use hyperdht::{
     cenc::Announce,
     crypto::{namespace, sign_announce_or_unannounce, Keypair2},
-    HyperDht, HyperDhtEvent,
+    request_announce_or_unannounce_value,
 };
 
 use common::{
-    js::{path_to_node_modules, require_js_data},
+    js::{make_repl, KEYPAIR_JS},
     Result,
 };
-use rusty_nodejs_repl::{Config, Repl};
+use rusty_nodejs_repl::Repl;
 
 const DEFAULT_SEED: [u8; 32] = [
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ];
-const KEYPAIR_JS: &str = "
-createKeyPair = require('hyperdht/lib/crypto.js').createKeyPair;
-seed = new Uint8Array(32);
-seed[0] = 1;
-keyPair = createKeyPair(seed)
-";
 
 fn sign_announce(
     keypair: &Keypair2,
@@ -47,22 +40,6 @@ fn buf_to_js_comparable_str(x: &[u8]) -> String {
         .map(|x| x.to_string())
         .collect::<Vec<String>>()
         .join(",")
-}
-
-async fn make_repl() -> Repl {
-    require_js_data().unwrap();
-
-    let mut conf = Config::build().unwrap();
-    conf.before.push(
-        "
-stringify = JSON.stringify;
-write = process.stdout.write.bind(process.stdout);
-writeJson = x => write(stringify(x))
-"
-        .into(),
-    );
-    conf.path_to_node_modules = Some(path_to_node_modules().unwrap().display().to_string());
-    conf.start().await.unwrap()
 }
 
 #[tokio::test]
@@ -245,10 +222,14 @@ write(stringify([...c.encode(m.announce, ann)]))
         .await?;
 
     let kp = Keypair2::from_seed(DEFAULT_SEED);
-    let announce = sign_announce(&kp, target.into(), &token, &from_id, &[])?;
-    use compact_encoding::types::CompactEncodable;
-    let mut buff: Vec<u8> = vec![0u8; CompactEncodable::encoded_size(&announce).unwrap()];
-    announce.encoded_bytes(&mut buff).unwrap();
+    let buff = request_announce_or_unannounce_value(
+        &kp,
+        target.into(),
+        &token,
+        from_id.into(),
+        &[],
+        &hyperdht::crypto::namespace::ANNOUNCE,
+    )?;
     assert_eq!(buff, expected);
     Ok(())
 }
@@ -293,103 +274,14 @@ write(stringify([...c.encode(m.announce, ann)]))
         .await?;
 
     let kp = Keypair2::from_seed(DEFAULT_SEED);
-    let announce = sign_announce(&kp, target.into(), &token, &from_id, &relay_addresses)?;
-    use compact_encoding::types::CompactEncodable;
-    let mut buff: Vec<u8> = vec![0u8; CompactEncodable::encoded_size(&announce).unwrap()];
-    announce.encoded_bytes(&mut buff).unwrap();
+    let buff = request_announce_or_unannounce_value(
+        &kp,
+        target.into(),
+        &token,
+        from_id.into(),
+        &relay_addresses,
+        &hyperdht::crypto::namespace::ANNOUNCE,
+    )?;
     assert_eq!(buff, expected);
-    Ok(())
-}
-
-use tracing_subscriber::EnvFilter;
-pub fn log() {
-    tracing_subscriber::fmt()
-        .with_target(true)
-        .with_line_number(true)
-        // print when instrumented funtion enters
-        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ENTER)
-        .with_file(true)
-        .with_env_filter(EnvFilter::from_default_env()) // Reads `RUST_LOG` environment variable
-        .without_time()
-        .init();
-}
-
-async fn other_node_socketadd(repl: &mut Repl) -> Result<SocketAddr> {
-    Ok(repl
-        .json_run::<String, _>(
-            "
-bs_node = testnet.nodes[1]
-write(stringify(`${bs_node.host}:${bs_node.port}`))
-",
-        )
-        .await?
-        .parse()?)
-}
-#[tokio::test]
-async fn testnet() -> Result<()> {
-    let mut repl = make_repl().await;
-    repl.run(KEYPAIR_JS).await?;
-    // Create testnet and get bootstrap address
-    repl.run(
-        "
-createTestnet = require('hyperdht/testnet.js');
-testnet = await createTestnet();
-",
-    )
-    .await?;
-
-    let bs_addr = other_node_socketadd(&mut repl).await?;
-
-    let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
-    while let Some(_x) = hdht.next().await {
-        if 2 * 2 == 4 {
-            break;
-        }
-    }
-
-    let topic: [u8; 32] = repl
-        .json_run(
-            "
-    const b4a = require('b4a')
-    topic = b4a.alloc(32);
-    topic.write('hello', 0);
-    write(stringify([...topic]))
-    ",
-        )
-        .await?;
-    log();
-
-    let _res = repl
-        .run(
-            "
-    ann_node = testnet.nodes[testnet.nodes.length - 1];
-    query = await ann_node.announce(topic, ann_node.defaultKeyPair);
-    await query.finished();
-    ",
-        )
-        .await?;
-
-    let _qid = hdht.lookup(topic.into(), hyperdht::Commit::No);
-
-    let mut public_key = None;
-    loop {
-        match hdht.next().await {
-            Some(HyperDhtEvent::LookupResult(res)) => {
-                if res.query_id == _qid {
-                    break;
-                }
-            }
-            Some(HyperDhtEvent::LookupResponse(resp)) => {
-                if let Some(x) = resp.peers.last() {
-                    let pk = public_key.get_or_insert(x.public_key);
-                    assert_eq!(*pk, x.public_key);
-                }
-            }
-            Some(_) => {}
-            None => panic!("when would this end?"),
-        }
-    }
-
-    assert!(public_key.is_some());
     Ok(())
 }
---
+++ hdht/tests/testnet.rs
@@ -0,0 +1,251 @@
+mod common;
+use std::{net::SocketAddr, time::Duration};
+
+use common::{
+    js::{make_repl, KEYPAIR_JS},
+    Result,
+};
+use dht_rpc::DhtConfig;
+use futures::StreamExt;
+use hyperdht::{crypto::Keypair2, HyperDht, HyperDhtEvent, QueryOpts};
+use rusty_nodejs_repl::Repl;
+use tracing_subscriber::EnvFilter;
+pub fn log() {
+    tracing_subscriber::fmt()
+        .with_target(true)
+        .with_line_number(true)
+        // print when instrumented funtion enters
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ENTER)
+        .with_file(true)
+        .with_env_filter(EnvFilter::from_default_env()) // Reads `RUST_LOG` environment variable
+        .without_time()
+        .init();
+}
+
+async fn other_node_socketadd(repl: &mut Repl) -> Result<SocketAddr> {
+    Ok(repl
+        .json_run::<String, _>(
+            "
+bs_node = testnet.nodes[1]
+write(stringify(`${bs_node.host}:${bs_node.port}`))
+",
+        )
+        .await?
+        .parse()?)
+}
+
+#[tokio::test]
+async fn testnet_lookup() -> Result<()> {
+    let mut repl = make_repl().await;
+    repl.run(KEYPAIR_JS).await?;
+    // Create testnet and get bootstrap address
+    repl.run(
+        "
+createTestnet = require('hyperdht/testnet.js');
+testnet = await createTestnet();
+",
+    )
+    .await?;
+
+    let bs_addr = other_node_socketadd(&mut repl).await?;
+    let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
+    while let Some(_x) = hdht.next().await {
+        if 2 * 2 == 4 {
+            break;
+        }
+    }
+
+    let topic: [u8; 32] = repl
+        .json_run(
+            "
+    const b4a = require('b4a')
+    topic = b4a.alloc(32);
+    topic.write('hello', 0);
+    write(stringify([...topic]))
+    ",
+        )
+        .await?;
+    log();
+
+    let _res = repl
+        .run(
+            "
+    ann_node = testnet.nodes[testnet.nodes.length - 1];
+    query = await ann_node.announce(topic, ann_node.defaultKeyPair);
+    await query.finished();
+    ",
+        )
+        .await?;
+
+    let _qid = hdht.lookup(topic.into(), hyperdht::Commit::No);
+
+    let mut public_key = None;
+    loop {
+        match hdht.next().await {
+            Some(HyperDhtEvent::LookupResult(res)) => {
+                if res.query_id == _qid {
+                    break;
+                }
+            }
+            Some(HyperDhtEvent::LookupResponse(resp)) => {
+                if let Some(x) = resp.peers.last() {
+                    let pk = public_key.get_or_insert(x.public_key);
+                    assert_eq!(*pk, x.public_key);
+                }
+            }
+            Some(_) => {}
+            None => panic!("when would this end?"),
+        }
+    }
+
+    assert!(public_key.is_some());
+    Ok(())
+}
+
+#[tokio::test]
+async fn net_announce() -> Result<()> {
+    let mut repl = make_repl().await;
+    repl.run(KEYPAIR_JS).await?;
+    // Create testnet and get bootstrap address
+    repl.run(
+        "
+createTestnet = require('hyperdht/testnet.js');
+testnet = await createTestnet();
+",
+    )
+    .await?;
+
+    let bs_addr = other_node_socketadd(&mut repl).await?;
+
+    let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
+    while let Some(_x) = hdht.next().await {
+        if 2 * 2 == 4 {
+            break;
+        }
+    }
+
+    let topic: [u8; 32] = repl
+        .json_run(
+            "
+    const b4a = require('b4a')
+    topic = b4a.alloc(32);
+    topic.write('hello', 0);
+    write(stringify([...topic]))
+    ",
+        )
+        .await?;
+
+    let kp = Keypair2::default();
+    let opts = QueryOpts::default();
+
+    let qid = hdht.announce(topic.into(), &kp, &[], &opts);
+
+    log();
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(Duration::from_secs(3)).await;
+            let what = repl.drain_stdout().await.unwrap();
+            let what_str = String::from_utf8(what).unwrap();
+            println!("JS STDOUT: {what_str}");
+        }
+    });
+    loop {
+        match hdht.next().await {
+            Some(e) => {
+                dbg!(&e);
+            }
+            None => panic!("when would this end?"),
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn verify_sign_announce() -> Result<()> {
+    let mut repl = make_repl().await;
+    repl.run(KEYPAIR_JS).await?;
+
+    repl.run(
+        "
+createTestnet = require('hyperdht/testnet.js');
+testnet = await createTestnet();
+",
+    )
+    .await?;
+
+    let bs_addr = other_node_socketadd(&mut repl).await?;
+    let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
+
+    let topic: [u8; 32] = repl
+        .json_run(
+            "
+    const b4a = require('b4a')
+    topic = b4a.alloc(32);
+    topic.write('hello', 0);
+    write(stringify([...topic]))
+    ",
+        )
+        .await?;
+
+    let _res = repl
+        .run(
+            "
+    ann_node = testnet.nodes[testnet.nodes.length - 1];
+    query = await ann_node.announce(topic, ann_node.defaultKeyPair);
+    await query.finished();
+    ",
+        )
+        .await?;
+
+    let _qid = hdht.lookup(topic.into(), hyperdht::Commit::No);
+
+    let mut r = None;
+    loop {
+        match hdht.next().await {
+            Some(HyperDhtEvent::LookupResult(res)) => {
+                dbg!();
+                if res.query_id == _qid {
+                    break;
+                }
+            }
+            Some(HyperDhtEvent::LookupResponse(resp)) => {
+                if let Some(t) = resp.response.token {
+                    r = Some(resp.response.clone());
+                }
+            }
+            Some(x) => {
+                //dbg!(x);
+            }
+            None => panic!("when would this end?"),
+        }
+    }
+
+    assert!(r.is_some());
+
+    r.inspect(|x| {
+        //dbg!(x);
+    });
+
+    /*
+        // in rust create the parts of the announce request
+        // token - usually you get this in a reply to a request. What kinda req?
+        //   a valid token will be included in a lookup request
+        // could just log the parts from rust. See QueryStreamType::commit
+        // components we need
+        let x = repl
+            .run(
+                "
+    Persistent = require('hyperdht/lib/persistent.js');
+    write(Persistent.prototype.verifyAnnounce.toString())
+    //writeJson(Persistent.verifyAnnounce({ value, target, token, }, nodeId))
+    ",
+            )
+            .await?;
+
+
+        println!("{}", String::from_utf8_lossy(&x));
+
+    */
+    todo!()
+}
