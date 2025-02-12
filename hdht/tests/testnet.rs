74fe09f2562201f413079932369cd154dcc446e7
Blake Griffith <blake.a.griffith@gmail.com>
2025-02-12 14:46:06 -0500

    refactor test utils

--- hdht/tests/testnet.rs
+++ hdht/tests/testnet.rs
@@ -3,41 +3,49 @@ use std::{net::SocketAddr, time::Duration};
 
 use common::{
     js::{make_repl, KEYPAIR_JS},
-    Result,
+    log, Result,
 };
 use dht_rpc::DhtConfig;
 use futures::StreamExt;
 use hyperdht::{crypto::Keypair2, HyperDht, HyperDhtEvent, QueryOpts};
 use rusty_nodejs_repl::Repl;
 use tracing_subscriber::EnvFilter;
-pub fn log() {
-    tracing_subscriber::fmt()
-        .with_target(true)
-        .with_line_number(true)
-        // print when instrumented funtion enters
-        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ENTER)
-        .with_file(true)
-        .with_env_filter(EnvFilter::from_default_env()) // Reads `RUST_LOG` environment variable
-        .without_time()
-        .init();
-}
 
-async fn other_node_socketadd(repl: &mut Repl) -> Result<SocketAddr> {
+fn show_bytes<T: AsRef<[u8]>>(x: T) {
+    println!("{}", String::from_utf8(x.as_ref().to_vec()).unwrap())
+}
+/// Get the address of a node from an existing testnet in JS
+/// NB: `testnet` must exist in the js context already
+async fn other_node_socketadd(repl: &mut Repl, node_index: usize) -> Result<SocketAddr> {
     Ok(repl
-        .json_run::<String, _>(
+        .json_run::<String, _>(format!(
             "
-bs_node = testnet.nodes[1]
-write(stringify(`${bs_node.host}:${bs_node.port}`))
-",
-        )
+bs_node = testnet.nodes[{node_index}]
+write(stringify(`${{bs_node.host}}:${{bs_node.port}}`))
+"
+        ))
         .await?
         .parse()?)
+}
+
+/// Create a target/topic. whith the argument `topic` written to to the beggining of the buffer,
+/// and padded with zeros. The variable in js is named "topic"
+async fn make_topic(repl: &mut Repl, topic: &str) -> Result<[u8; 32]> {
+    Ok(repl
+        .json_run(format!(
+            "
+    const b4a = require('b4a')
+    topic = b4a.alloc(32);
+    topic.write('{topic}', 0);
+    write(stringify([...topic]))
+    "
+        ))
+        .await?)
 }
 
 #[tokio::test]
 async fn testnet_lookup() -> Result<()> {
     let mut repl = make_repl().await;
-    repl.run(KEYPAIR_JS).await?;
     // Create testnet and get bootstrap address
     repl.run(
         "
@@ -47,7 +55,8 @@ testnet = await createTestnet();
     )
     .await?;
 
-    let bs_addr = other_node_socketadd(&mut repl).await?;
+    let bs_addr = other_node_socketadd(&mut repl, 1).await?;
+
     let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
     while let Some(_x) = hdht.next().await {
         if 2 * 2 == 4 {
@@ -55,18 +64,10 @@ testnet = await createTestnet();
         }
     }
 
-    let topic: [u8; 32] = repl
-        .json_run(
-            "
-    const b4a = require('b4a')
-    topic = b4a.alloc(32);
-    topic.write('hello', 0);
-    write(stringify([...topic]))
-    ",
-        )
-        .await?;
+    let topic = make_topic(&mut repl, "hello").await?;
     log();
 
+    dbg!();
     let _res = repl
         .run(
             "
@@ -87,8 +88,8 @@ testnet = await createTestnet();
                     break;
                 }
             }
-            Some(HyperDhtEvent::LookupResponse(resp)) => {
-                if let Some(x) = resp.peers.last() {
+            Some(HyperDhtEvent::LookupResponse(res)) => {
+                if let Some(x) = res.peers.last() {
                     let pk = public_key.get_or_insert(x.public_key.clone());
                     assert_eq!(*pk, x.public_key);
                 }
@@ -115,29 +116,19 @@ testnet = await createTestnet();
     )
     .await?;
 
-    let bs_addr = other_node_socketadd(&mut repl).await?;
-
+    let bs_addr = other_node_socketadd(&mut repl, 1).await?;
     let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
+
     while let Some(_x) = hdht.next().await {
         if 2 * 2 == 4 {
             break;
         }
     }
 
-    let topic: [u8; 32] = repl
-        .json_run(
-            "
-    const b4a = require('b4a')
-    topic = b4a.alloc(32);
-    topic.write('hello', 0);
-    write(stringify([...topic]))
-    ",
-        )
-        .await?;
+    let topic = make_topic(&mut repl, "hello").await?;
 
     let kp = Keypair2::default();
     let opts = QueryOpts::default();
-
     let qid = hdht.announce(topic.into(), &kp, &[], &opts);
 
     log();
@@ -161,10 +152,12 @@ testnet = await createTestnet();
     Ok(())
 }
 
+/// Do an "announce" from javascript, then have rust "lookup"
+/// In rust we get a public key in the lookup response, we check this is the same as the public key
+/// of the node that announced.
 #[tokio::test]
-async fn verify_sign_announce() -> Result<()> {
+async fn js_announces_rs_looksup() -> Result<()> {
     let mut repl = make_repl().await;
-    repl.run(KEYPAIR_JS).await?;
 
     repl.run(
         "
@@ -174,26 +167,17 @@ testnet = await createTestnet();
     )
     .await?;
 
-    let bs_addr = other_node_socketadd(&mut repl).await?;
+    let bs_addr = other_node_socketadd(&mut repl, 1).await?;
     let mut hdht = HyperDht::with_config(DhtConfig::default().add_bootstrap_node(bs_addr)).await?;
 
-    let topic: [u8; 32] = repl
-        .json_run(
-            "
-    const b4a = require('b4a')
-    topic = b4a.alloc(32);
-    topic.write('hello', 0);
-    write(stringify([...topic]))
-    ",
-        )
-        .await?;
+    let topic = make_topic(&mut repl, "hello").await?;
 
     let _res = repl
         .run(
             "
-    ann_node = testnet.nodes[testnet.nodes.length - 1];
-    query = await ann_node.announce(topic, ann_node.defaultKeyPair);
-    await query.finished();
+ann_node = testnet.nodes[testnet.nodes.length - 1];
+query = await ann_node.announce(topic, ann_node.defaultKeyPair);
+await query.finished();
     ",
         )
         .await?;
